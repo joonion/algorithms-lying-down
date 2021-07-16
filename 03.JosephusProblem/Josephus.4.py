ae2e5da8fe07f3191c13a2b6f9f06794534973f6
joonion <joonion@gmail.com>
2021-07-17 08:41:50 +0900

    Josephus Problem

--- 03.JosephusProblem/Josephus.4.py
+++ 03.JosephusProblem/Josephus.4.py
@@ -3,9 +3,6 @@
 
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        # if n == 1:
-        #     return 1
-        # return ((self.findTheWinner(n - 1, k) + k - 1) % n) + 1
         s = 1
         for n in range(1, n + 1):
             s = ((s + k - 1) % n) + 1
