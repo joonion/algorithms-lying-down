163c433f5736bc6823b5246c2b2130deb4340a19
Joonion Bae <joonion@gmail.com>
2022-04-17 10:46:32 +0900

    Activity Selection

--- ActivitySelection/selection.2.py
+++ ActivitySelection/selection.2.py
@@ -1,18 +1,17 @@
 # Greedy with Iteration
 
-def selection(s, f):
-    n = len(s) - 1
-    S = [1]
-    k = 1
-    for m in range(2, n + 1):
-        if s[m] >= f[k]:
-            S += [m]
-            k = m
+def selection(n, s, f):
+    S = []
+    F = 0
+    for k in range(1, n + 1):
+        if F <= s[k]:
+            S += [k]
+            F = f[k]
     return S
 
 n = int(input())
-s, f = [0] * (n+1), [0] * (n+1)
+s, f = [0] * (n + 1), [0] * (n + 1)
 for i in range(1, n + 1):
     s[i], f[i] = map(int, input().split())
-solution = selection(s, f)
+solution = selection(n, s, f)
 print(len(solution), solution)
