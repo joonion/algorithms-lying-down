1a2071fadc62f8293ebb7985f4eff4f44ef3f35a
Joonion Bae <joonion@gmail.com>
2022-04-17 09:51:30 +0900

    Activity Selection Problem

--- ActivitySelection/selection.4.py
+++ ActivitySelection/selection.4.py
@@ -4,7 +4,7 @@ def selection(n, s, f):
     C = [[0] * (n+2) for _ in range(n+2)]
     for i in range(1, n + 1):
         C[i][i] = 1
-    for d in range(1, n + 2):
+    for d in range(1, n + 1):
         for i in range(n - d + 1):
             j = i + d
             for k in range(i, j + 1):
@@ -16,10 +16,9 @@ n = int(input())
 s, f = [0] * (n+1), [0] * (n+1)
 for i in range(1, n + 1):
     s[i], f[i] = map(int, input().split())
-s, f = s + [max(f)], f + [max(f)]
 C = selection(n, s, f)
-for i in range(n + 2):
-    for j in range(n + 2):
+for i in range(n + 1):
+    for j in range(n + 1):
         print(C[i][j], end=" ")
     print()
 print(C[0][n])
