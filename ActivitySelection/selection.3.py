93d2260d4f9bd16ea0dfc71f1c0f82b33c0a763f
Joonion Bae <joonion@gmail.com>
2022-04-17 09:46:23 +0900

    ActivitySelection

--- ActivitySelection/selection.3.py
+++ ActivitySelection/selection.3.py
@@ -1,18 +1,16 @@
 # Divide-and-Conquer
 
 def selection(i, j, s, f):
-    if f[i] > s[j]:
-        return 0
-    else:
-        opt = 2
-        for k in range(i + 1, j):
-            if f[i] <= s[k] and f[k] <= s[j]:
-                opt = max(opt, selection(i, k, s, f) + selection(k, j, s, f) + 1)
-        return opt
+    opt = 0
+    for k in range(i + 1, j):
+        if f[i] <= s[k] and f[k] <= s[j]:
+            opt = max(opt, selection(i, k, s, f) + selection(k, j, s, f) + 1)
+    return opt
 
 n = int(input())
 s, f = [0] * (n+1), [0] * (n+1)
 for i in range(1, n + 1):
     s[i], f[i] = map(int, input().split())
-solution = selection(1, n, s, f)
+s, f = s + [max(f)], f + [max(f)]
+solution = selection(0, n + 1, s, f)
 print(solution)
--- ActivitySelection/selection.4.py
+++ ActivitySelection/selection.4.py
@@ -1,9 +1,12 @@
 # Dynamic Programming
 
 def selection(n, s, f):
-    C = [[0] * (n+1) for _ in range(n+1)]
+    C = [[0] * (n+2) for _ in range(n+2)]
     for i in range(1, n + 1):
-        for j in range(1, n + 1):
+        C[i][i] = 1
+    for d in range(1, n + 2):
+        for i in range(n - d + 1):
+            j = i + d
             for k in range(i, j + 1):
                 if f[i] <= s[k] and f[k] <= s[j]:
                     C[i][j] = max(C[i][j], C[i][k] + C[k][j] + 1)
@@ -13,8 +16,10 @@ n = int(input())
 s, f = [0] * (n+1), [0] * (n+1)
 for i in range(1, n + 1):
     s[i], f[i] = map(int, input().split())
+s, f = s + [max(f)], f + [max(f)]
 C = selection(n, s, f)
-for i in range(n + 1):
-    for j in range(n + 1):
+for i in range(n + 2):
+    for j in range(n + 2):
         print(C[i][j], end=" ")
     print()
+print(C[0][n])
