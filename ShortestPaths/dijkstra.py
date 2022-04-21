d740412a6bbedf00996bfbd1b3af4832c141ba7b
Joonion Bae <joonion@gmail.com>
2022-04-22 01:11:43 +0900

    Shortest Paths

---
+++ ShortestPaths/dijkstra.py
@@ -0,0 +1,33 @@
+# Dijkstra Algorithm
+
+def dijkstra(n, s, W):
+    path = [s] * n
+    dist = W[s]
+    found = [False] * n
+    found[s] = True
+    for _ in range(n - 1):
+        minlength = INF
+        for i in range(n):
+            if not found[i] and dist[i] < minlength:
+                minlength = dist[i]
+                vnear = i
+        found[vnear] = True
+        for i in range(n):
+            if not found[i] and dist[i] > dist[vnear] + W[vnear][i]:
+                dist[i] = dist[vnear] + W[vnear][i]
+                path[i] = vnear
+        print(found)
+    return path, dist
+
+INF = 999
+n, m = map(int, input().split())
+W = [[INF] * n for _ in range(n)]
+for i in range(n):
+    W[i][i] = 0
+for i in range(m):
+    u, v, w = map(int, input().split())
+    W[u][v] = w
+    
+path, dist = dijkstra(n, 0, W)
+print(path, dist)
+    
---
+++ ShortestPaths/floyd.py
@@ -0,0 +1,30 @@
+# Floyd Algorithm
+
+def floyd(n, W):
+    path = [[-1] * n for _ in range(n)]
+    dist = W
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                    path[i][j] = k
+    return path, dist                    
+
+INF = 999
+n, m = map(int, input().split())
+W = [[INF] * n for _ in range(n)]
+for i in range(n):
+    W[i][i] = 0
+for i in range(m):
+    u, v, w = map(int, input().split())
+    W[u][v] = w
+    
+path, dist = floyd(n, W)
+for i in range(n):
+    print(path[i])
+print()
+for i in range(n):
+    print(dist[i])
+
+    
